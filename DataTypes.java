138246eeb43adc66917262a1845afcbbab845ca1
Hakim Ahmed <hakim.ahmed@student.manchester.ac.uk>
2025-03-28 17:39:36 +0000

    started learning datatypes

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes { 
+    public static void main(String[] args) {
+        byte byte1 = 100;
+        System.out.println(byte1);
+        short short1 = 10000;
+        System.out.println(short1);
+        int int1 = 100000;
+        System.out.println(int1);
+        long long1 = 1000000000L;
+        System.out.println(long1);
+
+        float floaty = 3.33f;
+        System.out.println(floaty);
+        double doubly = 333.333333333333d;
+        System.out.println(doubly);
+    }
+}
