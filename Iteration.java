c3fcebc2e91d80aa7733c0226dacba1ba794e9d1
Hakim Ahmed <hakim.ahmed@student.manchester.ac.uk>
2025-04-05 15:47:38 +0100

    Completed iteration.jyava

--- Iteration.java
+++ Iteration.java
@@ -32,5 +32,78 @@ public class Iteration {
             }
             dice += 1;
         }
+
+
+        /*
+        for (statement 1; statement 2; statement 3) {
+        // code block to be executed
+        }
+
+        Statement 1 is executed (one time) before the execution of the code block.
+        Statement 2 defines the condition for executing the code block.
+        Statement 3 is executed (every time) after the code block has been executed.
+        */
+        
+        for (int k = 0; k < 5; k++) {
+            System.out.println(k);
+        }
+
+        for (int a = 0; a <= 10; a += 2) {
+            System.out.println(a);
+        }
+
+        // Nested loops
+        for (int p = 1; p <= 2; p++) {
+            System.out.println("Outer: " + p);
+            for (int q = 1; q <= 3; q++) {
+                System.out.println("Inner: " + q);
+            }
+        }
+
+        /*
+        There is also a "for-each" loop, which is used exclusively to loop through elements in an array (or other data sets):
+
+        for (type variableName : arrayName) {
+        // code block to be executed
+        }
+        */
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String b : cars) {
+            System.out.println(b);
+        }
+
+
+        for (int o = 0; o <= 10; o++) {
+            if (o == 4) {
+                break; // Breaks out of the loop when o = 4
+            }
+            System.out.println(o + "PF");
+        }
+
+        for (int n = 0; n <= 10; n++) {
+            if (n == 4) {
+                continue; // Continues to the next iteration (skips the 4th)
+            }
+            System.out.println(n + "PF");
+        }
+
+        int z = 0;
+        while (z < 10) {
+            System.out.println("Z: " + z);
+            z++;
+            if (z == 4) {
+                break;
+            }
+        }
+
+        int x = 0;
+        while (x < 10) {
+            x++;
+            if (x == 4) {
+                continue;
+            }
+            System.out.println("X: " + x);
+        }
     }
 }
