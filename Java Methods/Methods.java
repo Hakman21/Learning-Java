ac5dbf36333bb3ee54fe7029745c1abb3647ca5e
Hakim Ahmed <hakim.ahmed@student.manchester.ac.uk>
2025-04-08 15:01:16 +0100

    Completed methods up to the parameters part

---
+++ Java Methods/Methods.java
@@ -0,0 +1,14 @@
+public class Methods {
+    public static void my_method(String fname, int age) {
+        /*
+        my__method() is the name of the method
+        static means that the method belongs to the Main class and not an object of the Main class
+        void means that this method does not have a return value
+        */
+        System.out.println(fname + " just got raped! He was " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        my_method("Hakim", 19);
+    }
+}
