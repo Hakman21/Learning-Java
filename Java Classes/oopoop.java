8b68cd4237fd5523c3e1a319e740a50f55dd4dc7
Hakim Ahmed <hakim.ahmed@student.manchester.ac.uk>
2025-04-26 20:12:53 +0100

    Just a commit

---
+++ Java Classes/oopoop.java
@@ -0,0 +1,11 @@
+public class oopoop {
+    public static void main(String[] args) {
+        /*
+        OOP is faster and easier to execute
+        OOP provides a clear structure for the programs
+        OOP helps to keep the Java code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
+        OOP makes it possible to create full reusable applications with less code and shorter development time
+        */
+        System.out.println("Tuff - Never PMO");
+    }
+}
