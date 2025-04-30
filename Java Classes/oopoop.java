fa7ce4d90d09b7773f329c6fc990f304628549fd
Hakim Ahmed <hakim.ahmed@student.manchester.ac.uk>
2025-04-30 16:56:34 +0100

    completed oop start

--- Java Classes/oopoop.java
+++ Java Classes/oopoop.java
@@ -1,9 +1,19 @@
 public class oopoop {
-    int x = 5;
-    final int y = 3;
+    int x;
+    int y;
     int z;
+    int n;
+    String name;
+    // YOU MUST CREATE AN OBJECT OF THE CLASS TO CALL A PUBLIC. STATIC CAN BE ACESSED WHENEVER
+    public oopoop(int m, String eman) { //CONSTRUCTOR METHOD RUNS TO SET INTIAL x AND y values
+        x = 2;
+        y = 3;
+        n = m;
+        name = eman;
+    }
+
     public static void main(String[] args) {
-        oopoop myObj = new oopoop();
+        oopoop myObj = new oopoop(5, "H");
         System.out.println(myObj.x);
         myObj.x = 40;
         //myObj.y = 50; THIS WONT WORK BECAUSE FINAL 
@@ -11,11 +21,14 @@ public class oopoop {
         System.out.println(myObj.x);
         System.out.println(myObj.y);
         System.out.println(myObj.z);
+        System.out.println(myObj.n);
+        System.out.println(myObj.name);
 
         myMethod();     
         // myPublicMethod(); This would compile an error
-        oopoop myObj2 = new oopoop();
-        
+        oopoop myObj2 = new oopoop(5, "H");
+        myObj2.myPublicMethod();
+
     }
 
     static void myMethod() {
@@ -28,6 +41,13 @@ public class oopoop {
     }
 
     public void myPublicMethod() {
-        System.out.println("Public methods must be called by creating objects");
+        System.out.println("Public methods must be callby creating objects");
+    }
+
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
     }
 }
--- Java Classes/secondoopoop.java
+++ Java Classes/secondoopoop.java
@@ -1,6 +1,10 @@
 public class secondoopoop {
     public static void main(String[] args) {
-        oopoop myObj = new oopoop();
+        oopoop myObj = new oopoop(5, "H");
         System.out.println(myObj.x);
+
+        oopoop myCar = new oopoop(5, "H");
+        myCar.fullThrottle();
+        myCar.speed(300);
     }
 }
