24c9e7180ac663da243f3d17e5d300a391064f44
Hakim Ahmed <hakim.ahmed@student.manchester.ac.uk>
2025-04-30 18:15:04 +0100

    packages

---
+++ Java Classes/packages.java
@@ -0,0 +1,12 @@
+import java.util.*;
+//import package.name.Class;   // Import a single class
+//import package.name.*;   // Import the whole package
+//package mypack; use this to create my own package
+public class packages {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String username = input.nextLine();
+        System.out.println(username);
+    }
+}
