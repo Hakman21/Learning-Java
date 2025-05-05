eb97d61a1a192f865c5071fa19bfca4b8c0643ce
Hakim Ahmed <hakim.ahmed@student.manchester.ac.uk>
2025-05-05 13:55:52 +0100

    splash

---
+++ Java File Handling/files.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+public class files {
+    public static void main(String[] args) {
+        File file = new File("example.txt");
+
+        // canRead() 	Boolean 	Tests whether the file is readable or not
+        // canWrite() 	Boolean 	Tests whether the file is writable or not
+        // createNewFile() 	Boolean 	Creates an empty file
+        // delete() 	Boolean 	Deletes a file
+        // exists() 	Boolean 	Tests whether the file exists
+        // getName() 	String 	Returns the name of the file
+        // getAbsolutePath() 	String 	Returns the absolute pathname of the file
+        // length() 	Long 	Returns the size of the file in bytes
+        // list() 	String[] 	Returns an array of the files in the directory
+        // mkdir() 	Boolean 	Creates a directory
+
+    }
+}
